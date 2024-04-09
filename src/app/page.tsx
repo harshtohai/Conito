e16fdefe2c9f52390936ab94733bf7f3c3c28242
Harsh Panchal <harsh.panchal.me@gmail.com>
2024-04-09 14:51:55 +0530

    Footer and new features section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,7 +6,7 @@ export default function Home() {
       <Navbar />
       
       <div className="h-[96vh]">
-        <div className="bg-pribg h-[95.9vh] flex-col  rounded-[6px] flex items-center justify-center">
+        <div className="bg-pribg h-[96.4vh] flex-col mt-[1.3rem] rounded-[6px] flex items-center justify-center">
           <div className="flex flex-col justify-center items-center h-[90%]">
             <h2 className="text-text text-4xl flex ">I am <i className="italic font-medium ml-[6px]"> Conito</i> </h2>
             <h1 className="text-text text-5xl font-medium mb-[4px] flex items-center justify-center flex-wrap">
@@ -19,27 +19,32 @@ export default function Home() {
         </div>
       </div>
       <div className="h-[92.8vh]  rounded-b-[6px] flex flex-col items-end ">
-        <div className="h-[50px]  relative w-[25rem] mt-[-2.5rem] mr-[2rem] border-[3px] border-text flex justify-center bg-pribg rounded-[10px]">
+        <div className="h-[50px]  relative w-[25rem] mt-[-2.2rem] mr-[2rem] border-[3px] border-text flex justify-center bg-pribg rounded-[10px]">
           <h4 className="text-brstroke mt-[6px] text-xl ">Here have a look</h4>
         </div>
-        <div className=" flex z-10 mt-[-8px] h-full w-full rounded-[6px]">
+        <div className=" flex z-10 mt-[-9px] h-full w-full rounded-[6px]">
           <div className="basis-1/4 bg-pribg rounded-[6px]  rounded-br-[0px] h-full"></div>
-          <div className="bg-pribg w-[3px] h-[10rem] self-end"></div> 
+          <div className="bg-pribg w-[3px] h-[6rem] self-end"></div> 
           <div className="basis-3/4 bg-pribg rounded-[6px] rounded-bl-[0px] h-full"></div>
         </div>
       </div>
-      <div className="h-[98vh] w-full flex   rounded-[6px]">
-        <div className="basis-1/2 h-full flex flex-col mt-[3px]">
-          <div className="basis-1/2 bg-pribg rounded-[4px] rounded-br-[0px]"></div>
-          <div className="w-1/6 h-[3px] bg-pribg self-end"></div>
-          <div className="basis-1/2 bg-pribg rounded-[4px] rounded-tr-[0px]"></div>
-        </div>
-        <div className="h-2/6 bg-pribg w-[3px] self-center"></div>
-        <div className="basis-1/2 h-full flex flex-col mt-[3px]">
-          <div className="basis-1/2 bg-pribg rounded-[4px] rounded-bl-[0px]"></div>
-          <div className="w-1/6 h-[3px] bg-pribg "></div>
-          <div className="basis-1/2 bg-pribg rounded-[4px] rounded-tl-[0px]"></div>
-        </div>
+      <div className="h-[99.2vh]  mt-[3px] w-full flex flex-col rounded-[6px]">
+        <div className="h-1/5 w-full bg-pribg rounded-[6px] rounded-br-[0px]"></div>
+        <span className="h-[3px] w-[3rem] bg-pribg self-end "></span>
+        <div className="h-1/5 w-full bg-pribg rounded-[6px] rounded-tr-[0px] rounded-bl-[0px]"></div>
+        <span className="h-[3px] w-[3rem] bg-pribg "></span>
+        <div className="h-1/5 w-full bg-pribg rounded-[6px] rounded-tl-[0px] rounded-br-[0px]"></div>
+        <span className="h-[3px] w-[3rem] bg-pribg self-end "></span>
+        <div className="h-1/5 w-full bg-pribg rounded-[6px] rounded-tr-[0px] rounded-bl-[0px]"></div>
+        <span className="h-[3px] w-[3rem] bg-pribg "></span>
+        <div className="h-1/5 w-full bg-pribg rounded-[6px] rounded-tl-[0px]"></div>
+      </div>
+      <div className="w-full h-[30vh] flex mt-[3px]">
+        <div className="w-1/3 h-full bg-pribg rounded-[6px] rounded-tr-[0px] "></div>
+        <span className="h-1/5 w-[3px] self-start bg-pribg"></span>
+        <div className="w-1/3 h-full bg-pribg rounded-[6px] rounded-br-[0px] rounded-tl-[0px] "></div>
+        <span className="h-1/5 w-[3px] self-end bg-pribg"></span>
+        <div className="w-1/3 h-full bg-pribg rounded-[6px] rounded-bl-[0px] "></div>
       </div>
     </main>
   );
--- src/components/homepage/navbar/navbar.tsx
+++ src/components/homepage/navbar/navbar.tsx
@@ -3,7 +3,7 @@ import Navbutton from "./navbuttons";
 export default function Navbar() {
   return(
   <div className="z-20 absolute  h-[50px] flex ">
-    <nav className=" border-[3px] border-text bg-pribg ml-[2rem] rounded-b-[9px] w-[25rem]  px-[10px] py-[8px] text-line text-sm flex justify-between items-center ">   
+    <nav className=" border-[3px] border-text bg-pribg ml-[2rem] rounded-[9px] w-[25rem]  px-[10px] py-[8px] text-line text-sm flex justify-between items-center ">   
       <div className="flex text-white">
         <p className="">Home</p>
         <div className="h-2xl w-[2px] bg-zinc-700 mx-[10px] "></div>
