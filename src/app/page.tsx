c5246f563f36dab283dde95c01f985b04caade37
Harsh Panchal <harsh.panchal.me@gmail.com>
2024-04-10 01:20:05 +0530

    Removed features sectiona and wrote things in footer :hehehe:

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,5 @@
 import Navbar from "@/components/homepage/navbar/navbar";
-
+import Link from "next/link";
 export default function Home() {
   return (
     <main className="bg-text p-[3px]">
@@ -8,7 +8,7 @@ export default function Home() {
       <div className="h-[96vh]">
         <div className="bg-pribg h-[96.4vh] flex-col mt-[1.3rem] rounded-[6px] flex items-center justify-center">
           <div className="flex flex-col justify-center items-center h-[90%]">
-            <h2 className="text-text text-4xl flex ">I am <i className="italic font-medium ml-[6px]"> Conito</i> </h2>
+            <h2 className="text-text text-4xl flex ">Its <i className="italic font-medium ml-[6px]"> Conito</i> </h2>
             <h1 className="text-text text-5xl font-medium mb-[4px] flex items-center justify-center flex-wrap">
               Your Personalised AI Powered Coding Assistant
             </h1>
@@ -28,23 +28,32 @@ export default function Home() {
           <div className="basis-3/4 bg-pribg rounded-[6px] rounded-bl-[0px] h-full"></div>
         </div>
       </div>
-      <div className="h-[99.2vh]  mt-[3px] w-full flex flex-col rounded-[6px]">
-        <div className="h-1/5 w-full bg-pribg rounded-[6px] rounded-br-[0px]"></div>
-        <span className="h-[3px] w-[3rem] bg-pribg self-end "></span>
-        <div className="h-1/5 w-full bg-pribg rounded-[6px] rounded-tr-[0px] rounded-bl-[0px]"></div>
-        <span className="h-[3px] w-[3rem] bg-pribg "></span>
-        <div className="h-1/5 w-full bg-pribg rounded-[6px] rounded-tl-[0px] rounded-br-[0px]"></div>
-        <span className="h-[3px] w-[3rem] bg-pribg self-end "></span>
-        <div className="h-1/5 w-full bg-pribg rounded-[6px] rounded-tr-[0px] rounded-bl-[0px]"></div>
-        <span className="h-[3px] w-[3rem] bg-pribg "></span>
-        <div className="h-1/5 w-full bg-pribg rounded-[6px] rounded-tl-[0px]"></div>
-      </div>
       <div className="w-full h-[30vh] flex mt-[3px]">
-        <div className="w-1/3 h-full bg-pribg rounded-[6px] rounded-tr-[0px] "></div>
+        <div className="w-1/3 h-full py-[5vh] px-[5vh] bg-pribg rounded-[6px] rounded-tr-[0px] "> 
+          <p className="text-4xl mb-1 text-text font-medium italic">Conito</p>
+          <p>Beliver of Fuck Around & Find Out,  </p>
+          <p>so if you wanna know more login and test it :)</p>
+          <p className="text-xs text-btn">(fuck this font ruined my smily)</p>
+        </div>
         <span className="h-1/5 w-[3px] self-start bg-pribg"></span>
-        <div className="w-1/3 h-full bg-pribg rounded-[6px] rounded-br-[0px] rounded-tl-[0px] "></div>
+        <div className="w-1/3 h-full bg-pribg py-[6vh] px-[5vh] flex  items-center flex-col rounded-[6px] rounded-br-[0px] rounded-tl-[0px] ">
+          <p className="text-[19px] italic" >"Test out this Product and try to Contribute. Or I will call you earlier"</p>
+          <p className="self-end ">--Satan</p>
+        </div>
         <span className="h-1/5 w-[3px] self-end bg-pribg"></span>
-        <div className="w-1/3 h-full bg-pribg rounded-[6px] rounded-bl-[0px] "></div>
+        <div className="w-1/3 h-full p-[5vh] flex text-lg justify-between text-white  bg-pribg rounded-[6px] rounded-bl-[0px] "> 
+          <div className="flex justify-between w-full">
+            <Link href="https://github.com/harshtohai/conito">
+              GitHub
+            </Link> 
+            <Link href="https://x.com/conito">
+              X/(Twitter)
+            </Link>
+            <Link href="https://x.com/cognitowl">
+              Developers X/(Twitter)
+            </Link>
+          </div>
+        </div>
       </div>
     </main>
   );
--- src/components/homepage/navbar/navbar.tsx
+++ src/components/homepage/navbar/navbar.tsx
@@ -1,10 +1,9 @@
 import Navbutton from "./navbuttons";
-
-export default function Navbar() {
+export default function Navbar(){
   return(
   <div className="z-20 absolute  h-[50px] flex ">
     <nav className=" border-[3px] border-text bg-pribg ml-[2rem] rounded-[9px] w-[25rem]  px-[10px] py-[8px] text-line text-sm flex justify-between items-center ">   
-      <div className="flex text-white">
+      <div className="flex text-white"> 
         <p className="">Home</p>
         <div className="h-2xl w-[2px] bg-zinc-700 mx-[10px] "></div>
         <p>GitHub</p>
